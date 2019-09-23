16f53be06488ab00da79e62e4ca7e50be4eefeda
zc <zc@linkpi.cn>
2019-09-23 20:58:21 +0800

    switch to 3531D

--- Encoder.pro
+++ Encoder.pro
@@ -4,7 +4,7 @@ QT -= gui
 TARGET = Encoder
 CONFIG += console  c++14
 CONFIG -= app_bundle
-chip = HI3559A
+chip = HI3531D
 include(../../LinkLib/Link.pri)
 TEMPLATE = app
 
--- Version.h
+++ Version.h
@@ -2,7 +2,7 @@
 #define VERSION
 
 #define VERSION_VER "1.0.0"
-#define VERSION_BUILD 283
-#define VERSION_DATE "20190918"
+#define VERSION_BUILD 285
+#define VERSION_DATE "20190923"
 #endif // VERSION
 
