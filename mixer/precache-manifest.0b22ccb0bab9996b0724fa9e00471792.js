eb5a4ca9faa72a48ca68c4911eed875d759f4bbb
wangc <21639061@qq.com>
2021-06-21 15:54:23 +0800

    布局管理器稳定版

---
+++ mixer/precache-manifest.0b22ccb0bab9996b0724fa9e00471792.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "794e9c34d0e5c7601d7d28db10a5a582",
+    "url": "./mixer/index.html"
+  },
+  {
+    "revision": "3bb86ce8163ba4a4564d",
+    "url": "./mixer/static/css/2.07b1449e.chunk.css"
+  },
+  {
+    "revision": "e867f432ccf9319cfac9",
+    "url": "./mixer/static/css/main.5e92fc36.chunk.css"
+  },
+  {
+    "revision": "3bb86ce8163ba4a4564d",
+    "url": "./mixer/static/js/2.5848e0e6.chunk.js"
+  },
+  {
+    "revision": "caad8f647ce261172d90c10189d6dcd2",
+    "url": "./mixer/static/js/2.5848e0e6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e867f432ccf9319cfac9",
+    "url": "./mixer/static/js/main.c27d310a.chunk.js"
+  },
+  {
+    "revision": "dfe4d10935ce0a2db166a373b806744c",
+    "url": "./mixer/static/js/main.c27d310a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "96af6c8d9af51963848a",
+    "url": "./mixer/static/js/runtime-main.c45edf70.js"
+  }
+]);
